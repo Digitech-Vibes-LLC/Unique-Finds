4a58ee2dea470e934b9803c8583648d2d6d0640e
dolagi <102247096+dolagi@users.noreply.github.com>
2024-10-19 22:15:58 +0300

    Update product.py

--- product_coding/models/product.py
+++ product_coding/models/product.py
@@ -58,16 +58,17 @@ class code(models.TransientModel):
 
 
     def change(self) :
-        products = self.env['product.product'].search([('default_code', '!=',False)])
+        products = self.env['product.product'].search([],order="default_code")
         for line in products :
-            last_code = line.default_code.split("-")
-            _logger.info("code>>>>>>>>>>>>>..2 %s",last_code[2])
-
-            # if isinstance(last_code[2], int):
-            #if int(last_code[2]):
-                #line.product_code = None
-            line.product_code = line.default_code
-            _logger.info("line.product_code>>>>>>>>>>>>>..2 %s",line.product_code)
+            if line.default_code :
+                last_code = line.default_code.split("-")
+                _logger.info("code>>>>>>>>>>>>>..2 %s",last_code[2])
+    
+                # if isinstance(last_code[2], int):
+                #if int(last_code[2]):
+                    #line.product_code = None
+                line.product_code = last_code[2]
+                _logger.info("line.product_code>>>>>>>>>>>>>..2 %s",line.product_code)
         # if self.categ_id :
         #     products = self.env['product.product'].search([('categ_id', '=', self.categ_id.id)])
         # code = 1000
